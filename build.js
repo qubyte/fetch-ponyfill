6fa86b129a1de8b8faa37fe99596afec0382b108
Mark S. Everitt <mark.s.everitt@gmail.com>
2016-08-21 22:36:08 +0100

    Updates the build script to pass ESLint.

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function indent(line) {
-    return line === '' ? '' : '      ' + line;
+  return line === '' ? '' : '      ' + line;
 }
 
 var fs = require('fs');
@@ -13,7 +13,11 @@ var whatwgFetchSource = fs.readFileSync(require.resolve('whatwg-fetch'), 'utf8')
 var wrapperSource = fs.readFileSync(require.resolve('./fetch-browser'), 'utf8');
 
 // Indent and place the fetch source inside the wrapper.
-var indented = whatwgFetchSource.split('\n').map(indent).join('\n');
+var indented = whatwgFetchSource
+  .split('\n')
+  .map(indent)
+  .join('\n');
+
 var builtSource = wrapperSource.replace('// {{whatwgFetch}}', indented);
 
-console.log(builtSource);
+console.log(builtSource); // eslint-disable-line no-console
