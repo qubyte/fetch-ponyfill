a505b8889b1ea21d5c4050285649120ec3ee5abe
Mark S. Everitt <mark.s.everitt@gmail.com>
2016-08-21 22:37:27 +0100

    Updates browser wrapper to return fetch and the assiciated constructors.

--- tests/fetch-browser.spec.js
+++ tests/fetch-browser.spec.js
@@ -9,11 +9,10 @@ function responseToText(response) {
 
 describe('fetch in browser', function () {
   var sandbox;
-  var server;
-  var requests;
 
   beforeEach(function () {
-    sandbox = sinon.sandbox.create({ useFakeServer: true });
+    sandbox = sinon.sandbox.create({useFakeServer: true});
+    sandbox.server.autoRespond = true;
   });
 
   afterEach(function () {
@@ -22,20 +21,29 @@ describe('fetch in browser', function () {
 
   describe('when called without a context', function () {
     var promise;
+    var fetch;
 
     beforeEach(function () {
-      var fetch = fetchWrapper();
+      fetch = fetchWrapper();
       sandbox.server.respondWith('https://blah.com/hello.world', 'Some response text.');
-      promise = fetch('https://blah.com/hello.world');
-      sandbox.server.respond();
+      sandbox.server.respondWith('https://blah.com/goodbye.world', 'Some other response text.');
+      promise = fetch.fetch('https://blah.com/hello.world');
+    });
+
+    it('exposes fetch, and Request, Response, and Headers methods', function () {
+      assert.deepEqual(Object.keys(fetch).sort(), ['Headers', 'Request', 'Response', 'fetch']);
+    });
+
+    it('returns a native promise instance which resolves to an instance of Response', function () {
+      assert.ok(promise instanceof Promise);
+
+      return promise.then(function (res) {
+        assert.ok(res instanceof fetch.Response);
+      });
     });
 
     it('makes requests', function () {
       assert.equal(sandbox.server.requests.length, 1);
-    });
-
-    it('returns a promise', function () {
-      assert.ok(promise instanceof Promise);
     });
 
     it('resolves when the server responds', function () {
@@ -43,26 +51,43 @@ describe('fetch in browser', function () {
         .then(responseToText)
         .then(function (data) {
           assert.equal(data, 'Some response text.');
+        });
+    });
+
+    it('can consume Request instances', function () {
+      return fetch.fetch(new fetch.Request('https://blah.com/goodbye.world'))
+        .then(responseToText)
+        .then(function (data) {
+          assert.equal(data, 'Some other response text.');
         });
     });
   });
 
   describe('when called with a context without a Promise field', function () {
     var promise;
+    var fetch;
 
     beforeEach(function () {
-      var fetch = fetchWrapper({});
+      fetch = fetchWrapper({});
       sandbox.server.respondWith('https://blah.com/hello.world', 'Some response text.');
-      promise = fetch('https://blah.com/hello.world');
-      sandbox.server.respond();
+      sandbox.server.respondWith('https://blah.com/goodbye.world', 'Some other response text.');
+      promise = fetch.fetch('https://blah.com/hello.world');
+    });
+
+    it('exposes fetch, and Request, Response, and Headers methods', function () {
+      assert.deepEqual(Object.keys(fetch).sort(), ['Headers', 'Request', 'Response', 'fetch']);
+    });
+
+    it('returns a native promise instance which resolves to an instance of Response', function () {
+      assert.ok(promise instanceof Promise);
+
+      return promise.then(function (res) {
+        assert.ok(res instanceof fetch.Response);
+      });
     });
 
     it('makes requests', function () {
       assert.equal(sandbox.server.requests.length, 1);
-    });
-
-    it('returns a promise', function () {
-      assert.ok(promise instanceof Promise);
     });
 
     it('resolves when the server responds', function () {
@@ -72,24 +97,41 @@ describe('fetch in browser', function () {
           assert.equal(data, 'Some response text.');
         });
     });
+
+    it('can consume Request instances', function () {
+      return fetch.fetch(new fetch.Request('https://blah.com/goodbye.world'))
+        .then(responseToText)
+        .then(function (data) {
+          assert.equal(data, 'Some other response text.');
+        });
+    });
   });
 
   describe('when called with a context with a Promise field', function () {
     var promise;
+    var fetch;
 
     beforeEach(function () {
-      var fetch = fetchWrapper({Promise: ThenPromise});
+      fetch = fetchWrapper({Promise: ThenPromise});
       sandbox.server.respondWith('https://blah.com/hello.world', 'Some response text.');
-      promise = fetch('https://blah.com/hello.world');
-      sandbox.server.respond();
+      sandbox.server.respondWith('https://blah.com/goodbye.world', 'Some other response text.');
+      promise = fetch.fetch('https://blah.com/hello.world');
+    });
+
+    it('exposes fetch, and Request, Response, and Headers methods', function () {
+      assert.deepEqual(Object.keys(fetch).sort(), ['Headers', 'Request', 'Response', 'fetch']);
+    });
+
+    it('returns an instance of the given promise constructor which resolves to an instance of Response', function () {
+      assert.ok(promise instanceof ThenPromise);
+
+      return promise.then(function (res) {
+        assert.ok(res instanceof fetch.Response);
+      });
     });
 
     it('makes requests', function () {
       assert.equal(sandbox.server.requests.length, 1);
-    });
-
-    it('returns an instance of the given Promise constructor', function () {
-      assert.ok(promise instanceof ThenPromise);
     });
 
     it('resolves when the server responds', function () {
@@ -97,6 +139,14 @@ describe('fetch in browser', function () {
         .then(responseToText)
         .then(function (data) {
           assert.equal(data, 'Some response text.');
+        });
+    });
+
+    it('can consume Request instances', function () {
+      return fetch.fetch(new fetch.Request('https://blah.com/goodbye.world'))
+        .then(responseToText)
+        .then(function (data) {
+          assert.equal(data, 'Some other response text.');
         });
     });
   });
